c76273180c7fd77e92d2d8caa9a1d575fa0a2ba6
Franco <f.furfuri@alumno.um.edu.ar>
2025-06-15 22:31:51 -0300

    se agregaron los test

---
+++ src/test.py
@@ -0,0 +1,53 @@
+import unittest
+from datetime import datetime
+from modelo import Paciente, Medico, Especialidad
+from clinica import Clinica
+
+class TestClinica(unittest.TestCase):
+    def setUp(self):
+        self.clinica = Clinica()
+        self.paciente = Paciente("Franco Furfuri", "12345678", "01/01/1990")
+        self.medico = Medico("Dra. Gómez", "M001")
+        self.especialidad = Especialidad("Clínica", ["lunes", "miércoles"])
+
+        self.clinica.agregar_paciente(self.paciente)
+        self.clinica.agregar_medico(self.medico)
+        self.medico.agregar_especialidad(self.especialidad)
+
+    def test_agregar_paciente(self):
+        self.assertIn("12345678", self.clinica.pacientes)
+
+    def test_agregar_medico(self):
+        self.assertIn("M001", self.clinica.medicos)
+
+    def test_agendar_turno_exitoso(self):
+        fecha = datetime.strptime("17/06/2025 09:00", "%d/%m/%Y %H:%M")  
+        resultado = self.clinica.agendar_turno("12345678", "M001", "Clínica", fecha)
+        self.assertFalse(resultado)  
+
+        fecha = datetime.strptime("16/06/2025 09:00", "%d/%m/%Y %H:%M")  
+        resultado = self.clinica.agendar_turno("12345678", "M001", "Clínica", fecha)
+        self.assertTrue(resultado)
+
+    def test_turno_duplicado(self):
+        fecha = datetime.strptime("16/06/2025 10:00", "%d/%m/%Y %H:%M")
+        self.clinica.agendar_turno("12345678", "M001", "Clínica", fecha)
+        resultado = self.clinica.agendar_turno("12345678", "M001", "Clínica", fecha)
+        self.assertFalse(resultado)
+
+    def test_emitir_receta_valida(self):
+        resultado = self.clinica.emitir_receta("12345678", "M001", ["Paracetamol", "Ibuprofeno"])
+        self.assertTrue(resultado)
+
+    def test_emitir_receta_sin_medicamentos(self):
+        resultado = self.clinica.emitir_receta("12345678", "M001", [])
+        self.assertFalse(resultado)
+
+    def test_historia_clinica(self):
+        self.clinica.emitir_receta("12345678", "M001", ["Vitamina C"])
+        historia = self.clinica.obtener_historia_clinica("12345678")
+        self.assertIsNotNone(historia)
+        self.assertEqual(len(historia.recetas), 1)
+
+if __name__ == '__main__':
+    unittest.main()
